01dff8b1d282d2f27fb414a0afdabb9f00217923
Hriday Desai <hridaydesai@Mac.lan>
2026-05-20 21:20:54 +0530

    feat: add login authentication and protected routes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -49,3 +49,33 @@ export const registerUser = async (req, res) => {
     });
   }
 };
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (
+      user &&
+      (await bcrypt.compare(password, user.password))
+    ) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+export const getCurrentUser = async (req, res) => {
+  res.json(req.user);
+};
